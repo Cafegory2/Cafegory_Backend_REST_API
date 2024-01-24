6ee73ab37e7b08fb16857ca223e674479bbbdd24
robinjoon <robin980108@naver.com>
2024-01-24 23:51:49 +0900

    feat! : 카공 생성 서비스 레이어 메서드 구현
    현재 개발 시점에서 카페 조회 레퍼지토리의 형태를 알 수 없으므로 Wrapper 를 정의하여 사용했다.

--- src/main/java/com/example/demo/dto/StudyOnceCreateRequest.java
+++ src/main/java/com/example/demo/dto/StudyOnceCreateRequest.java
@@ -1,4 +1,16 @@
 package com.example.demo.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class StudyOnceCreateRequest {
+	private long cafeId;
+	private String name;
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+	private int maxMemberCount;
+	private boolean canTalk;
 }
+
--- src/main/java/com/example/demo/service/StudyOnceServiceImpl.java
+++ src/main/java/com/example/demo/service/StudyOnceServiceImpl.java
@@ -1,17 +1,43 @@
 package com.example.demo.service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.Address;
+import com.example.demo.domain.CafeImpl;
+import com.example.demo.domain.StudyOnceImpl;
 import com.example.demo.dto.PagedResponse;
 import com.example.demo.dto.StudyOnceCreateRequest;
 import com.example.demo.dto.StudyOnceSearchRequest;
 import com.example.demo.dto.StudyOnceSearchResponse;
 import com.example.demo.dto.UpdateAttendanceResponse;
+import com.example.demo.repository.CafeRepositoryWrapper;
+import com.example.demo.repository.StudyOnceRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class StudyOnceServiceImpl implements StudyOnceService {
+	private final CafeRepositoryWrapper cafeRepositoryWrapper = id -> {
+		CafeImpl cafe = CafeImpl.builder()
+			.id(id)
+			.address(new Address())
+			.isOpen(true)
+			.phone("010-1234-5678")
+			.name("테스트 카페")
+			.snsDetails(Collections.emptyList())
+			.businessHours(Collections.emptyList())
+			.avgReviewRate(4.5)
+			.maxAllowableStay(1)
+			.build();
+		return Optional.of(cafe);
+	};
+	private final StudyOnceRepository studyOnceRepository;
+
 	@Override
 	public void tryJoin(String memberIdThatExpectedToJoin, String studyId) {
 
@@ -39,6 +65,39 @@ public class StudyOnceServiceImpl implements StudyOnceService {
 
 	@Override
 	public StudyOnceSearchResponse createStudy(String leaderId, StudyOnceCreateRequest studyOnceCreateRequest) {
-		return null;
+		CafeImpl cafe = cafeRepositoryWrapper.findById(studyOnceCreateRequest.getCafeId()).orElseThrow();
+		StudyOnceImpl studyOnce = makeNewStudyOnce(studyOnceCreateRequest, cafe);
+		StudyOnceImpl saved = studyOnceRepository.save(studyOnce);
+		boolean canJoin = true;
+		return makeStudyOnceSearchRespone(saved, canJoin);
+	}
+
+	private static StudyOnceImpl makeNewStudyOnce(StudyOnceCreateRequest studyOnceCreateRequest, CafeImpl cafe) {
+		return StudyOnceImpl.builder()
+			.name(studyOnceCreateRequest.getName())
+			.startDateTime(studyOnceCreateRequest.getStartDateTime())
+			.endDateTime(studyOnceCreateRequest.getEndDateTime())
+			.maxMemberCount(studyOnceCreateRequest.getMaxMemberCount())
+			.nowMemberCount(0)
+			.isEnd(false)
+			.canTalk(studyOnceCreateRequest.isCanTalk())
+			.cafe(cafe)
+			.build();
+
+	}
+
+	private static StudyOnceSearchResponse makeStudyOnceSearchRespone(StudyOnceImpl saved, boolean canJoin) {
+		return StudyOnceSearchResponse.builder()
+			.cafeId(saved.getCafe().getId())
+			.id(saved.getId())
+			.name(saved.getName())
+			.startDateTime(saved.getStartDateTime())
+			.endDateTime(saved.getEndDateTime())
+			.maxMemberCount(saved.getMaxMemberCount())
+			.nowMemberCount(saved.getNowMemberCount())
+			.canTalk(saved.isCanTalk())
+			.canJoin(canJoin)
+			.isEnd(saved.isEnd())
+			.build();
 	}
 }
