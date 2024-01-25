b1ba29726bf8a1547ebde0988fb87c17f0948b73
robinjoon <robin980108@naver.com>
2024-01-25 21:38:39 +0900

    Test : 카공 목록 조회 테스트 코드 추가

--- src/test/java/com/example/demo/service/StudyOnceServiceImplTest.java
+++ src/test/java/com/example/demo/service/StudyOnceServiceImplTest.java
@@ -1,20 +1,58 @@
 package com.example.demo.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.example.demo.dto.PagedResponse;
 import com.example.demo.dto.StudyOnceCreateRequest;
+import com.example.demo.dto.StudyOnceSearchRequest;
 import com.example.demo.dto.StudyOnceSearchResponse;
 
 @SpringBootTest
 class StudyOnceServiceImplTest {
 	@Autowired
 	StudyOnceServiceImpl studyOnceService;
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	@DisplayName("정상 목록 조회 테스트")
+	void searchStudyByDto() {
+		//given
+		initCafe();
+		LocalDateTime start = LocalDateTime.now().plusHours(4).plusMinutes(1);
+		LocalDateTime end = start.plusHours(1);
+		long cafeId = 1L;
+		long leaderId = 1L;
+		StudyOnceCreateRequest studyOnceCreateRequest = makeStudyOnceCreateRequest(start, end, cafeId);
+
+		//when
+		StudyOnceSearchResponse expectedStudyOnceSearchResponse = studyOnceService.createStudy(leaderId,
+			studyOnceCreateRequest);
+		StudyOnceSearchRequest studyOnceSearchRequest = new StudyOnceSearchRequest("테스트동");
+		PagedResponse<StudyOnceSearchResponse> pagedResponse = studyOnceService.searchStudy(studyOnceSearchRequest);
+
+		//then
+		List<StudyOnceSearchResponse> results = pagedResponse.getResults();
+		org.assertj.core.api.Assertions.assertThat(results).contains(expectedStudyOnceSearchResponse);
+	}
+
+	private void initCafe() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(
+			"INSERT INTO cafe "
+				+ "(cafe_id, full_address, region, avg_review_rate, max_allowable_stay, name, phone)"
+				+ " VALUES (1, 'a', '테스트동', 1, 10, '테스트 카페', '010-1234-5678')");
+	}
 
 	@Test
 	@DisplayName("정상 조회 테스트")
