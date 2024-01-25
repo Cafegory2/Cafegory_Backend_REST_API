c94ef2e641cc66f37fee70ea495282df7d9445d5
robinjoon <robin980108@naver.com>
2024-01-25 14:44:34 +0900

    Test : StudyOnceService 테스트 코드 추가

--- src/test/java/com/example/demo/service/StudyOnceServiceImplTest.java
+++ src/test/java/com/example/demo/service/StudyOnceServiceImplTest.java
@@ -20,7 +20,7 @@ class StudyOnceServiceImplTest {
 	@DisplayName("정상 생성 테스트")
 	void create() {
 		LocalDateTime start = LocalDateTime.now();
-		LocalDateTime end = start.minusHours(3);
+		LocalDateTime end = start.plusHours(3);
 		long cafeId = 1L;
 		long leaderId = 1L;
 		StudyOnceCreateRequest studyOnceCreateRequest = new StudyOnceCreateRequest(cafeId, "테스트 카페", start, end, 4,
@@ -28,6 +28,45 @@ class StudyOnceServiceImplTest {
 		StudyOnceSearchResponse result = studyOnceService.createStudy(leaderId, studyOnceCreateRequest);
 		StudyOnceSearchResponse expected = makeExpectedStudyOnceCreateResult(cafeId, studyOnceCreateRequest, result);
 		Assertions.assertEquals(expected, result);
+	}
+
+	@Test
+	@DisplayName("카공 시작 시간이 현재 시간 + 3시간 보다 이전인 경우 실패")
+	void createFailByStartTime() {
+		LocalDateTime start = LocalDateTime.now().plusHours(3).minusSeconds(1);
+		LocalDateTime end = start.plusHours(3);
+		long cafeId = 1L;
+		long leaderId = 1L;
+		StudyOnceCreateRequest studyOnceCreateRequest = new StudyOnceCreateRequest(cafeId, "테스트 카페", start, end, 4,
+			true);
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> studyOnceService.createStudy(leaderId, studyOnceCreateRequest));
+	}
+
+	@Test
+	@DisplayName("카공 시작 시간과 종료시간 차이가 1시간 미만일 경우 실패")
+	void createFailByStartTimeAndEndTimePeriodIsTooShort() {
+		LocalDateTime start = LocalDateTime.now().minusSeconds(1);
+		LocalDateTime end = start.plusMinutes(59);
+		long cafeId = 1L;
+		long leaderId = 1L;
+		StudyOnceCreateRequest studyOnceCreateRequest = new StudyOnceCreateRequest(cafeId, "테스트 카페", start, end, 4,
+			true);
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> studyOnceService.createStudy(leaderId, studyOnceCreateRequest));
+	}
+
+	@Test
+	@DisplayName("카공 시작 시간과 종료시간 차이가 5시간 초과일 경우 실패")
+	void createFailByStartTimeAndEndTimePeriodIsTooLong() {
+		LocalDateTime start = LocalDateTime.now().minusSeconds(1);
+		LocalDateTime end = start.plusHours(5).minusSeconds(1);
+		long cafeId = 1L;
+		long leaderId = 1L;
+		StudyOnceCreateRequest studyOnceCreateRequest = new StudyOnceCreateRequest(cafeId, "테스트 카페", start, end, 4,
+			true);
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> studyOnceService.createStudy(leaderId, studyOnceCreateRequest));
 	}
 
 	private static StudyOnceSearchResponse makeExpectedStudyOnceCreateResult(long cafeId,
