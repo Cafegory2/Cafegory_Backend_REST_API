ecc142927fe7377e114db4fcde6e769a0b5693f7
robinjoon <robin980108@naver.com>
2024-01-25 15:53:06 +0900

    feat : 카공 하나 조회, 카공 생성 컨트롤러 구현

--- src/main/java/com/example/demo/controller/StudyOnceController.java
+++ src/main/java/com/example/demo/controller/StudyOnceController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
-import java.util.List;
+import java.util.Collections;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,25 +14,33 @@ import com.example.demo.dto.PagedResponse;
 import com.example.demo.dto.StudyOnceCreateRequest;
 import com.example.demo.dto.StudyOnceSearchRequest;
 import com.example.demo.dto.StudyOnceSearchResponse;
+import com.example.demo.service.StudyOnceService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/study/once")
+@RequiredArgsConstructor
 public class StudyOnceController {
-	@GetMapping("/study/once/{studyId:[0-9]+}")
-	public ResponseEntity<StudyOnceSearchResponse> search(Long studyId) {
-		return ResponseEntity.ok(new StudyOnceSearchResponse());
+	private final StudyOnceService studyOnceService;
+
+	@GetMapping("/{studyId:[0-9]+}")
+	public ResponseEntity<StudyOnceSearchResponse> search(@PathVariable Long studyId) {
+		StudyOnceSearchResponse response = studyOnceService.searchByStudyId(studyId);
+		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("/study/once/list}")
+	@GetMapping("/list")
 	public ResponseEntity<PagedResponse<StudyOnceSearchResponse>> searchList(
 		StudyOnceSearchRequest studyOnceSearchRequest) {
 		PagedResponse<StudyOnceSearchResponse> pagedResponse = new PagedResponse<>(1, 1, 1,
-			List.of(new StudyOnceSearchResponse()));
+			Collections.emptyList());
 		return ResponseEntity.ok(pagedResponse);
 	}
 
-	@PostMapping("/study/once")
-	public ResponseEntity<StudyOnceSearchResponse> create(StudyOnceCreateRequest studyOnceCreateRequest) {
-		return ResponseEntity.ok(new StudyOnceSearchResponse());
+	@PostMapping("")
+	public ResponseEntity<StudyOnceSearchResponse> create(@RequestBody StudyOnceCreateRequest studyOnceCreateRequest) {
+		StudyOnceSearchResponse response = studyOnceService.createStudy(1L, studyOnceCreateRequest);
+		return ResponseEntity.ok(response);
 	}
 }
