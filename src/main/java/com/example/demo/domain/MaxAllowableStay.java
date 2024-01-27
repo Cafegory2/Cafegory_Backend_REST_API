f58291694049bb88fe1ee1a7e5b9199b1dab6f0f
donghyunkim <ghffu405@gmail.com>
2024-01-27 13:07:55 +0900

    rename: 변수명 수정

--- src/main/java/com/example/demo/domain/MaxAllowableStay.java
+++ src/main/java/com/example/demo/domain/MaxAllowableStay.java
@@ -7,14 +7,14 @@ import java.util.stream.Collectors;
 
 public enum MaxAllowableStay {
 
-	IRRELEVANT(0, hour -> true),
-	ONE_HOUR(1, hour -> isLittleOrEqualThanHour(1, hour) || isZero(hour)),
-	TWO_HOUR(2, hour -> isLittleOrEqualThanHour(2, hour) || isZero(hour)),
-	THREE_HOUR(3, hour -> isLittleOrEqualThanHour(3, hour) || isZero(hour)),
-	FOUR_HOUR(4, hour -> isLittleOrEqualThanHour(4, hour) || isZero(hour)),
-	FIVE_HOUR(5, hour -> isLittleOrEqualThanHour(5, hour) || isZero(hour)),
-	SIX_HOUR(6, hour -> isLittleOrEqualThanHour(6, hour) || isZero(hour)),
-	OVER_SIX_HOUR(7, hour -> equals(7, hour) || isZero(hour));
+	IRRELEVANT(0, maxHour -> true),
+	ONE_HOUR(1, maxHour -> isLittleOrEqualThanHour(1, maxHour) || isZero(maxHour)),
+	TWO_HOUR(2, maxHour -> isLittleOrEqualThanHour(2, maxHour) || isZero(maxHour)),
+	THREE_HOUR(3, maxHour -> isLittleOrEqualThanHour(3, maxHour) || isZero(maxHour)),
+	FOUR_HOUR(4, maxHour -> isLittleOrEqualThanHour(4, maxHour) || isZero(maxHour)),
+	FIVE_HOUR(5, maxHour -> isLittleOrEqualThanHour(5, maxHour) || isZero(maxHour)),
+	SIX_HOUR(6, maxHour -> isLittleOrEqualThanHour(6, maxHour) || isZero(maxHour)),
+	OVER_SIX_HOUR(7, maxHour -> equals(7, maxHour) || isZero(maxHour));
 
 	private final int value;
 	private Predicate<Integer> predicate;
