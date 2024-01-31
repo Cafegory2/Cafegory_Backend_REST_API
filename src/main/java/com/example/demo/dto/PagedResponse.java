9988a9a8b17f84e505bc89658816fe2d7455be30
donghyunkim <ghffu405@gmail.com>
2024-01-31 17:08:52 +0900

    fix: api응답 문서와 일치하지 않은 변수명 수정

--- src/main/java/com/example/demo/dto/PagedResponse.java
+++ src/main/java/com/example/demo/dto/PagedResponse.java
@@ -9,7 +9,7 @@ public class PagedResponse<T> {
 	private final int nowPage;
 	private final int maxPage;
 	private final int pageSize;
-	private final List<T> results;
+	private final List<T> list;
 
 	public static <T> PagedResponse<T> createWithFirstPageAsOne(int nowPage, int maxPage, int pageSize,
 		List<T> results) {
