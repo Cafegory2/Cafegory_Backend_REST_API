c877e4636df3b64a3617c337b5e00ed3e689100f
donghyunkim <ghffu405@gmail.com>
2024-01-31 17:06:00 +0900

    fix: api응답 문서와 일치하지 않은 변수명 수정

--- src/main/java/com/example/demo/dto/PagedResponse.java
+++ src/main/java/com/example/demo/dto/PagedResponse.java
@@ -8,11 +8,11 @@ import lombok.Data;
 public class PagedResponse<T> {
 	private final int nowPage;
 	private final int maxPage;
-	private final int sizePerPage;
+	private final int pageSize;
 	private final List<T> results;
 
-	public static <T> PagedResponse<T> createWithFirstPageAsOne(int nowPage, int maxPage, int sizePerPage,
+	public static <T> PagedResponse<T> createWithFirstPageAsOne(int nowPage, int maxPage, int pageSize,
 		List<T> results) {
-		return new PagedResponse<>(nowPage + 1, maxPage, sizePerPage, results);
+		return new PagedResponse<>(nowPage + 1, maxPage, pageSize, results);
 	}
 }
