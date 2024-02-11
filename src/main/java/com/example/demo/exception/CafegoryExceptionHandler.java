f534f30f8bd1b1e6910fa1458a1b49f306f86155
robinjoon <robin980108@naver.com>
2024-02-11 16:40:39 +0900

    fix : 예외 발생 시 예외 스택트레이스를 출력하도록 수정

--- src/main/java/com/example/demo/exception/CafegoryExceptionHandler.java
+++ src/main/java/com/example/demo/exception/CafegoryExceptionHandler.java
@@ -1,15 +1,23 @@
 package com.example.demo.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.demo.dto.ErrorResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class CafegoryExceptionHandler {
 	@ExceptionHandler(CafegoryException.class)
 	public ResponseEntity<ErrorResponse> handle(CafegoryException exception) {
+		PrintWriter printWriter = new PrintWriter(new StringWriter());
+		exception.printStackTrace(printWriter);
 		ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
 		return ResponseEntity.status(exception.getHttpStatus()).body(errorResponse);
 	}
