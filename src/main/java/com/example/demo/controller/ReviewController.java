475c1dd0127c8a1c9ac542767b129674e23676a8
donghyunkim <ghffu405@gmail.com>
2024-02-13 22:33:34 +0900

    feat: 리뷰 삭제 api 구현

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -57,6 +58,16 @@ public class ReviewController {
 		long identityId = cafegoryTokenManager.getIdentityId(authorization);
 		reviewService.updateReview(identityId, reviewId, reviewUpdateRequest);
 		ReviewResponse response = reviewQueryService.searchOne(reviewId);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/cafe/review/{reviewId}")
+	public ResponseEntity<ReviewResponse> deleteReview(@PathVariable Long reviewId,
+		@RequestHeader("Authorization") String authorization) {
+
+		long identityId = cafegoryTokenManager.getIdentityId(authorization);
+		ReviewResponse response = reviewQueryService.searchOne(reviewId);
+		reviewService.deleteReview(identityId, reviewId);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
