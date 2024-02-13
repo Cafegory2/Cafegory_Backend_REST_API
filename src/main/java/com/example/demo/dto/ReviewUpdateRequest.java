590ea549ff4659c6221869be6f1c22167ddf8cc2
donghyunkim <ghffu405@gmail.com>
2024-02-13 22:34:28 +0900

    fix: commit 하지 않은 클래스 추가

---
+++ src/main/java/com/example/demo/dto/ReviewUpdateRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
+import lombok.Getter;
+
+@Getter
+public class ReviewUpdateRequest {
+
+	@NotBlank
+	private String content;
+	@PositiveOrZero
+	private double rate;
+
+	public ReviewUpdateRequest() {
+	}
+
+	public ReviewUpdateRequest(String content, double rate) {
+		this.content = content;
+		this.rate = rate;
+	}
+}
