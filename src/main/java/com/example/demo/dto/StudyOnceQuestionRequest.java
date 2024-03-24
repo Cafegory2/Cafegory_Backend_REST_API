be2b2ae39f315863c997c2be463fcbb586debf8d
donghyunkim <ghffu405@gmail.com>
2024-03-24 20:33:07 +0900

    feat: bean validation notblank 추가

--- src/main/java/com/example/demo/dto/StudyOnceQuestionRequest.java
+++ src/main/java/com/example/demo/dto/StudyOnceQuestionRequest.java
@@ -1,10 +1,13 @@
 package com.example.demo.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Getter;
 
 @Getter
 public class StudyOnceQuestionRequest {
 
+	@NotBlank
 	private String content;
 
 	public StudyOnceQuestionRequest() {
