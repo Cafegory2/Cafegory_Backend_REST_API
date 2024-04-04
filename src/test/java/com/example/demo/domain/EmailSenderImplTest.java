64cc5d15e76760321be666e89745a599e0faf2c4
donghyunkim <ghffu405@gmail.com>
2024-04-04 22:02:28 +0900

    fix: 실제 api 호출 테스트 주석처리

--- src/test/java/com/example/demo/domain/EmailSenderImplTest.java
+++ src/test/java/com/example/demo/domain/EmailSenderImplTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.domain;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,9 +9,11 @@ class EmailSenderImplTest {
 	@Autowired
 	private EmailSender emailSender;
 
+	/*
+	실제 api 호출 테스트
 	@Test
 	void sendSimpleMessage() {
 		emailSender.sendSimpleMessage("ghffu405@gmail.com", "테스트 제목", "테스트 내용");
 	}
-
+	 */
 }
