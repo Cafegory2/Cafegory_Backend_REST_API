d89dea5ae187c4cfab2e5bcb0c020a3cf90c6da1
donghyunkim <ghffu405@gmail.com>
2024-05-15 02:00:39 +0900

    test: DayOfWeek에 맞는 영업시간을 찾는다

--- src/test/java/com/example/demo/domain/cafe/CafeTest.java
+++ src/test/java/com/example/demo/domain/cafe/CafeTest.java
@@ -3,12 +3,16 @@ package com.example.demo.domain.cafe;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import com.example.demo.domain.review.Review;
 
@@ -48,5 +52,37 @@ class CafeTest {
 			.build();
 		OptionalDouble rating = cafe.calcAverageRating();
 		assertTrue(rating.isEmpty());
+	}
+
+	@ParameterizedTest
+	@EnumSource(value = DayOfWeek.class)
+	@DisplayName("DayOfWeek에 맞는 영업시간을 찾는다.")
+	void findBusinessHour(DayOfWeek dayOfWeek) {
+		List<BusinessHour> businessHours = makeBusinessHourWith7daysFrom9To21();
+		Cafe cafe = Cafe.builder()
+			.businessHours(businessHours)
+			.build();
+
+		BusinessHour businessHour = cafe.findBusinessHour(dayOfWeek);
+
+		assertAll(
+			() -> assertThat(businessHour.getStartTime()).isEqualTo(LocalTime.of(9, 0)),
+			() -> assertThat(businessHour.getEndTime()).isEqualTo(LocalTime.of(21, 0))
+		);
+	}
+
+	private List<BusinessHour> makeBusinessHourWith7daysFrom9To21() {
+		List<String> daysOfWeek = List.of("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY");
+		List<BusinessHour> businessHours = new ArrayList<>();
+		for (String day : daysOfWeek) {
+			businessHours.add(
+				BusinessHour.builder()
+					.day(day)
+					.startTime(LocalTime.of(9, 0))
+					.endTime(LocalTime.of(21, 0))
+					.build()
+			);
+		}
+		return businessHours;
 	}
 }
