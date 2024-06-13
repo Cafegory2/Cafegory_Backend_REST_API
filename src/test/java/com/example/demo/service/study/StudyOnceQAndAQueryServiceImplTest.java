ffa2c96b9201ce8babe7b2323b76851643420eb7
donghyunkim <ghffu405@gmail.com>
2024-06-14 01:17:39 +0900

    refactor: StudyOnceQAndAQueryServiceImpl 스프링부트테스트로 전환

--- src/test/java/com/example/demo/service/study/StudyOnceQAndAQueryServiceImplTest.java
+++ src/test/java/com/example/demo/service/study/StudyOnceQAndAQueryServiceImplTest.java
@@ -1,28 +1,51 @@
 package com.example.demo.service.study;
 
+import javax.transaction.Transactional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+import com.example.demo.config.TestConfig;
 import com.example.demo.domain.cafe.Cafe;
 import com.example.demo.domain.member.Member;
+import com.example.demo.domain.member.ThumbnailImage;
 import com.example.demo.domain.study.StudyOnce;
 import com.example.demo.domain.study.StudyOnceComment;
 import com.example.demo.dto.study.StudyOnceCommentResponse;
-import com.example.demo.repository.study.InMemoryStudyOnceCommentRepository;
-import com.example.demo.service.ServiceTest;
+import com.example.demo.helper.save.CafeSaveHelper;
+import com.example.demo.helper.save.MemberSaveHelper;
+import com.example.demo.helper.save.StudyOnceCommentSaveHelper;
+import com.example.demo.helper.save.StudyOnceSaveHelper;
+import com.example.demo.helper.save.ThumbnailImageSaveHelper;
 
-class StudyOnceQAndAQueryServiceImplTest extends ServiceTest {
-	private final InMemoryStudyOnceCommentRepository studyOnceCommentRepository =
-		InMemoryStudyOnceCommentRepository.INSTANCE;
-	private final StudyOnceQAndAQueryService studyOnceQAndAQueryService = new StudyOnceQAndAQueryServiceImpl(
-		studyOnceCommentRepository, memberMapper, studyOnceMapper, studyOnceCommentMapper);
+@SpringBootTest
+@Import({TestConfig.class})
+@Transactional
+class StudyOnceQAndAQueryServiceImplTest {
+
+	@Autowired
+	private CafeSaveHelper cafePersistHelper;
+	@Autowired
+	private MemberSaveHelper memberPersistHelper;
+	@Autowired
+	private StudyOnceSaveHelper studyOncePersistHelper;
+	@Autowired
+	private StudyOnceCommentSaveHelper studyOnceCommentPersistHelper;
+	@Autowired
+	private ThumbnailImageSaveHelper thumbnailImagePersistHelper;
+	@Autowired
+	private StudyOnceQAndAQueryService studyOnceQAndAQueryService;
 
 	@Test
 	@DisplayName("카공 질문을 찾는다")
 	void searchQuestion() {
-		Member leader = memberPersistHelper.persistMemberWithName(THUMBNAIL_IMAGE, "카공장");
-		Member otherPerson = memberPersistHelper.persistMemberWithName(THUMBNAIL_IMAGE, "김동현");
+		ThumbnailImage thumbnailImage = thumbnailImagePersistHelper.persistDefaultThumbnailImage();
+		Member leader = memberPersistHelper.persistMemberWithName(thumbnailImage, "카공장");
+		Member otherPerson = memberPersistHelper.persistMemberWithName(thumbnailImage, "김동현");
 		Cafe cafe = cafePersistHelper.persistDefaultCafe();
 		StudyOnce studyOnce = studyOncePersistHelper.persistDefaultStudyOnce(cafe, leader);
 		StudyOnceComment studyOnceComment = studyOnceCommentPersistHelper.persistDefaultStudyOnceQuestion(
