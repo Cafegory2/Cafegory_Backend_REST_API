55d44bad225f7273f8ec249c59bfa856dfa1deea
donghyunkim <ghffu405@gmail.com>
2024-06-17 00:36:56 +0900

    refactor: StudyMemberTest 리팩토링

--- src/test/java/com/example/demo/domain/study/StudyMemberTest.java
+++ src/test/java/com/example/demo/domain/study/StudyMemberTest.java
@@ -1,61 +1,50 @@
 package com.example.demo.domain.study;
 
+import static com.example.demo.factory.TestMemberFactory.*;
+import static com.example.demo.factory.TestStudyMemberFactory.*;
+import static com.example.demo.factory.TestStudyOnceFactory.*;
+import static org.assertj.core.api.Assertions.*;
+
 import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import com.example.demo.domain.member.Member;
-import com.example.demo.factory.TestMemberFactory;
-import com.example.demo.factory.TestStudyMemberFactory;
-import com.example.demo.factory.TestStudyOnceFactory;
-
-import lombok.RequiredArgsConstructor;
 
 class StudyMemberTest {
+
 	private static final LocalDateTime BASE_TIME = LocalDateTime.now();
 
-	@RequiredArgsConstructor
-	private static class TimeInterval {
-		private final LocalDateTime start;
-		private final LocalDateTime end;
-
-		@Override
-		public String toString() {
-			return start.toString() + " ~ " + end.toString();
-		}
+	@ParameterizedTest
+	@MethodSource("isConflictParameters")
+	@DisplayName("주어진 시간과 겹치는지 확인한다.")
+	void check_if_times_overlap(LocalDateTime givenStartTime, LocalDateTime givenEndTime, boolean expected) {
+		//given
+		LocalDateTime start = BASE_TIME.plusHours(4);
+		LocalDateTime end = BASE_TIME.plusHours(7);
+		Member leader = createMember();
+		StudyOnce studyOnce = createStudyOnceWithTimeAndLeader(start, end, leader);
+		Member member = createMember();
+		StudyMember sut = createStudyMember(member, studyOnce);
+		//when
+		boolean isConflict = sut.isConflictWith(givenStartTime, givenEndTime);
+		//then
+		assertThat(isConflict).isEqualTo(expected);
 	}
 
 	static Stream<Arguments> isConflictParameters() {
 		return Stream.of(
-			Arguments.of(new TimeInterval(BASE_TIME, BASE_TIME.plusHours(4).minusSeconds(1)), false),
-			Arguments.of(new TimeInterval(BASE_TIME, BASE_TIME.plusHours(4)), true),
-			Arguments.of(new TimeInterval(BASE_TIME.plusHours(5), BASE_TIME.plusHours(6)), true),
-			Arguments.of(new TimeInterval(BASE_TIME.plusHours(5), BASE_TIME.plusHours(7)), true),
-			Arguments.of(new TimeInterval(BASE_TIME.plusHours(5), BASE_TIME.plusHours(8)), true),
-			Arguments.of(new TimeInterval(BASE_TIME.plusHours(7), BASE_TIME.plusHours(9)), true),
-			Arguments.of(new TimeInterval(BASE_TIME.plusHours(7).plusSeconds(1), BASE_TIME.plusHours(9)), false)
+			Arguments.of(BASE_TIME, BASE_TIME.plusHours(4).minusSeconds(1), false),
+			Arguments.of(BASE_TIME, BASE_TIME.plusHours(4), true),
+			Arguments.of(BASE_TIME.plusHours(5), BASE_TIME.plusHours(6), true),
+			Arguments.of(BASE_TIME.plusHours(5), BASE_TIME.plusHours(7), true),
+			Arguments.of(BASE_TIME.plusHours(5), BASE_TIME.plusHours(8), true),
+			Arguments.of(BASE_TIME.plusHours(7), BASE_TIME.plusHours(9), true),
+			Arguments.of(BASE_TIME.plusHours(7).plusSeconds(1), BASE_TIME.plusHours(9), false)
 		);
-	}
-
-	@ParameterizedTest
-	@MethodSource("isConflictParameters")
-	@DisplayName("카공 시작 시간이 주어진 시간의 끝과 겹치는 경우")
-	void isConflict(TimeInterval timeInterval, boolean expected) {
-		LocalDateTime start = BASE_TIME.plusHours(4);
-		LocalDateTime end = BASE_TIME.plusHours(7);
-		Member leader = TestMemberFactory.createMember();
-		StudyOnce studyOnce = TestStudyOnceFactory.createStudyOnceWithTimeAndLeader(start, end, leader);
-		Member member = TestMemberFactory.createMember();
-		StudyMember studyMember = TestStudyMemberFactory.createStudyMember(member, studyOnce);
-
-		boolean actual = studyMember.isConflictWith(timeInterval.start, timeInterval.end);
-
-		Assertions.assertThat(actual)
-			.isEqualTo(expected);
 	}
 }
