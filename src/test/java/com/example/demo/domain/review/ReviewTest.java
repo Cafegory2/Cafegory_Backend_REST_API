5093bab7b2bdae2c0a2ffb529fcbb46d5a295cec
donghyunkim <ghffu405@gmail.com>
2024-06-17 00:36:33 +0900

    chore: import 추가

--- src/test/java/com/example/demo/domain/review/ReviewTest.java
+++ src/test/java/com/example/demo/domain/review/ReviewTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.review;
 
+import static com.example.demo.factory.TestReviewFactory.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,44 +10,43 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import com.example.demo.exception.CafegoryException;
-import com.example.demo.factory.TestReviewFactory;
 
 class ReviewTest {
 
 	@Test
 	@DisplayName("null 검증")
-	void validateNull() {
-		assertThatThrownBy(() -> TestReviewFactory.createReviewWithContent(null))
+	void validate_null() {
+		assertThatThrownBy(() -> createReviewWithContent(null))
 			.isInstanceOf(CafegoryException.class);
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"", " "})
 	@DisplayName("빈값, 공백문자 검증")
-	void validateEmptyOrWhitespace(String value) {
-		assertThatThrownBy(() -> TestReviewFactory.createReviewWithContent(value))
+	void validate_empty_or_whitespace(String value) {
+		assertThatThrownBy(() -> createReviewWithContent(value))
 			.isInstanceOf(CafegoryException.class);
 	}
 
 	@ParameterizedTest
 	@ValueSource(doubles = {0, 5})
 	@DisplayName("평점 정상 범위 검증")
-	void validateRateRange(double number) {
-		assertDoesNotThrow(() -> TestReviewFactory.createReviewWithContentAndRate("a", number));
+	void validate_rate_range(double number) {
+		assertDoesNotThrow(() -> createReviewWithContentAndRate("a", number));
 	}
 
 	@ParameterizedTest
 	@ValueSource(doubles = {-0.1, 5.1})
 	@DisplayName("평점 예외 범위 검증")
-	void validateRateRange_exception(double number) {
-		assertThatThrownBy(() -> TestReviewFactory.createReviewWithContentAndRate("a", number))
+	void validate_rate_range_exception(double number) {
+		assertThatThrownBy(() -> createReviewWithContentAndRate("a", number))
 			.isInstanceOf(CafegoryException.class);
 	}
 
 	@Test
 	@DisplayName("리뷰 업데이트시, 리뷰 최대 글자수 검증")
 	void validate_update_content_size() {
-		Review review = TestReviewFactory.createReviewWithContentAndRate("리뷰", 5);
+		Review review = createReviewWithContentAndRate("리뷰", 5);
 
 		assertDoesNotThrow(() -> {
 			review.updateContent("a".repeat(200));
@@ -56,7 +56,7 @@ class ReviewTest {
 	@Test
 	@DisplayName("리뷰 업데이트시, 리뷰 최대 글자수 예외 검증")
 	void validate_update_content_size_exception() {
-		Review review = TestReviewFactory.createReviewWithContentAndRate("리뷰", 5);
+		Review review = createReviewWithContentAndRate("리뷰", 5);
 
 		assertThatThrownBy(() -> {
 			review.updateContent("a".repeat(201));
