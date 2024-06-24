395121361618b7e88144d2d03f853ef8bf5e986e
donghyunkim <ghffu405@gmail.com>
2024-06-24 11:44:49 +0900

    chore: import 추가

--- src/test/java/com/example/demo/service/study/StudyOnceCommentQueryServiceImplTest.java
+++ src/test/java/com/example/demo/service/study/StudyOnceCommentQueryServiceImplTest.java
@@ -1,10 +1,10 @@
 package com.example.demo.service.study;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,7 +65,7 @@ class StudyOnceCommentQueryServiceImplTest {
 		List<StudyOnceCommentSearchResponse> comments = commentsSearchResponse.getComments();
 		StudyOnceCommentSearchResponse actualQuestion = comments.get(0);
 		StudyOnceReplyResponse actualReply = actualQuestion.getReplies().get(0);
-		Assertions.assertAll(
+		assertAll(
 			() -> assertThat(actualQuestion.getQuestionInfo().getComment()).isEqualTo("질문"),
 			() -> assertThat(actualReply.getComment()).isEqualTo("답변")
 		);
