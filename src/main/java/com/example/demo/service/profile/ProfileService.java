0399c913f613e54356a2c480c2fef7fd88d177d7
donghyunkim <ghffu405@gmail.com>
2024-07-06 16:25:03 +0900

    fix: ProfileService의 default메서드에 @Transactional 추가

--- src/main/java/com/example/demo/service/profile/ProfileService.java
+++ src/main/java/com/example/demo/service/profile/ProfileService.java
@@ -2,11 +2,15 @@ package com.example.demo.service.profile;
 
 import java.time.LocalDateTime;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.example.demo.dto.profile.ProfileGetResponse;
 import com.example.demo.dto.profile.ProfileUpdateRequest;
 import com.example.demo.dto.profile.ProfileUpdateResponse;
 
 public interface ProfileService {
+
+	@Transactional
 	default ProfileGetResponse get(Long requestMemberId, Long targetMemberId) {
 		return get(requestMemberId, targetMemberId, LocalDateTime.now());
 	}
