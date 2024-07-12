fdb230196466fe65053fcb8e881aedaf45d4bc9b
donghyunkim <ghffu405@gmail.com>
2024-07-12 14:20:33 +0900

    refactor: 코드 정리

--- src/test/java/com/example/demo/utils/DatabaseCleanup.java
+++ src/test/java/com/example/demo/utils/DatabaseCleanup.java
@@ -17,59 +17,39 @@ public class DatabaseCleanup {
 
 	@PersistenceContext
 	private EntityManager em;
-
 	private List<String> tableNames;
 
 	@PostConstruct
 	public void afterPropertiesSet() {
-		// tableNames = em.getMetamodel().getEntities().stream()
-		// 	.filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
-		// 	.map(e -> convertCamelCaseToSnakeCase(e.getName()))
-		// 	.collect(Collectors.toList());
 		tableNames = em.getMetamodel().getEntities().stream()
 			.map(this::getTableName)
 			.collect(Collectors.toList());
 	}
 
 	private String getTableName(EntityType<?> entityType) {
-		String tableName;
 		Class<?> javaType = entityType.getJavaType();
-
 		// @Table 어노테이션이 있는 경우 테이블 이름 가져오기
 		if (javaType.isAnnotationPresent(Table.class)) {
 			Table table = javaType.getAnnotation(Table.class);
-			tableName = table.name();
-		} else {
-			// @Table 어노테이션이 없는 경우 엔티티 이름에서 테이블 이름으로 변환
-			tableName = convertCamelCaseToSnakeCase(entityType.getName());
+			return table.name();
 		}
-
-		return tableName;
+		// @Table 어노테이션이 없는 경우 엔티티 이름에서 테이블 이름으로 변환
+		return convertCamelCaseToSnakeCase(entityType.getName());
 	}
 
 	@Transactional
 	public void execute() {
 		em.flush();
-		afterPropertiesSet();
-		// em.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
 
 		for (String tableName : tableNames) {
 			em.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
 		}
-
-		// em.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
 	}
 
-	private static String convertCamelCaseToSnakeCase(String input) {
+	private String convertCamelCaseToSnakeCase(String input) {
 		if (input == null) {
 			return null;
 		}
-
-		String regex = "([a-z])([A-Z]+)";
-		String replacement = "$1_$2";
-
-		return input
-			.replaceAll(regex, replacement)
-			.toLowerCase();
+		return input.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
 	}
 }
