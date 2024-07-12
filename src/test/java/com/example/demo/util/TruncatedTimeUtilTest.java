f50d6049305c69495f9c52dd27a57f0bcc75c21e
donghyunkim <ghffu405@gmail.com>
2024-07-12 23:35:27 +0900

    rename: 테스트 메서드명 변경

--- src/test/java/com/example/demo/util/TruncatedTimeUtilTest.java
+++ src/test/java/com/example/demo/util/TruncatedTimeUtilTest.java
@@ -12,14 +12,14 @@ class TruncatedTimeUtilTest {
 
 	@Test
 	@DisplayName("나노초에서 초단위로 절삭한다.")
-	void convert_nano_to_micro_time() {
+	void convert_nano_to_second_time() {
 		LocalTime result = TruncatedTimeUtil.truncateTimeToSecond(LocalTime.of(23, 59, 59));
 		assertThat(result).isEqualTo(LocalTime.of(23, 59, 59));
 	}
 
 	@Test
 	@DisplayName("나노초에서 초단위로 절삭한다.")
-	void convert_nano_to_micro_date_time() {
+	void convert_nano_to_second_date_time() {
 		LocalDateTime result = TruncatedTimeUtil.truncateDateTimeToSecond(LocalDateTime.MAX);
 		assertThat(result).isEqualTo(LocalDateTime.of(999999999, 12, 31, 23, 59, 59));
 	}
