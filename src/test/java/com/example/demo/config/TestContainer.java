8792c3d300cf995701cbcce700b7b9726447fa8f
donghyunkim <ghffu405@gmail.com>
2024-07-19 23:06:27 +0900

    test: protected 접근제한자로 변경

--- src/test/java/com/example/demo/config/TestContainer.java
+++ src/test/java/com/example/demo/config/TestContainer.java
@@ -6,7 +6,7 @@ import org.testcontainers.containers.MariaDBContainer;
 
 public abstract class TestContainer {
 
-	private static final MariaDBContainer mariadbContainer;
+	protected static MariaDBContainer mariadbContainer;
 
 	static {
 		mariadbContainer = new MariaDBContainer("mariadb:10.11");
@@ -14,7 +14,7 @@ public abstract class TestContainer {
 	}
 
 	@DynamicPropertySource
-	private static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+	protected static void registerDynamicProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.datasource.url", mariadbContainer::getJdbcUrl);
 		registry.add("spring.datasource.username", mariadbContainer::getUsername);
 		registry.add("spring.datasource.password", mariadbContainer::getPassword);
