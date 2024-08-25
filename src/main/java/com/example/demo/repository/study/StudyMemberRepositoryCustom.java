d482abaa202876331070e94e4d4aa5fe1e835f72
doha <dohaahi@naver.com>
2024-08-25 22:03:41 +0900

    chore: StudyMemberRepositoryCustom에서 예외 발생하여 메서드 주석 처리

--- src/main/java/com/example/demo/repository/study/StudyMemberRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/study/StudyMemberRepositoryCustom.java
@@ -1,11 +1,5 @@
 package com.example.demo.repository.study;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import com.example.demo.domain.member.Member;
-import com.example.demo.domain.study.CafeStudyMember;
-
 public interface StudyMemberRepositoryCustom {
-	List<CafeStudyMember> findByMemberAndStudyDate(Member member, LocalDate studyDate);
+	// List<CafeStudyMember> findByMemberAndStudyDate(Member member, LocalDate studyDate);
 }
