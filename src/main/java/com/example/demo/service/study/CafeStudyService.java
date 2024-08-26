a6743ecbae69f7b19f46011d87160eccf6c356e6
doha <dohaahi@naver.com>
2024-08-26 11:37:43 +0900

    refactor: StudyOnceService -> CafeStudyService 이름 변경

---
+++ src/main/java/com/example/demo/service/study/CafeStudyService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.study;
+
+public interface CafeStudyService {
+	// void tryJoin(long memberIdThatExpectedToJoin, long studyId);
+	//
+	// void tryQuit(long memberIdThatExpectedToQuit, long studyId);
+	//
+	// PagedResponse<StudyOnceSearchListResponse> searchStudy(StudyOnceSearchRequest studyOnceSearchRequest);
+	//
+	// StudyOnceSearchResponse searchByStudyId(long studyId);
+	//
+	// StudyOnceSearchResponse searchStudyOnceWithMemberParticipation(long studyOnceId, long memberId);
+	//
+	// UpdateAttendanceResponse updateAttendances(long leaderId, long studyOnceId,
+	// 	UpdateAttendanceRequest request, LocalDateTime now);
+	//
+	// void updateAttendance(long leaderId, long studyOnceId, long memberId, Attendance attendance, LocalDateTime now);
+
+	// CafeStudyCreateResponse createStudy(long leaderId, CafeStudyCreateRequest cafeStudyCreateRequest);
+
+	// Long changeCafe(Long requestMemberId, Long studyOnceId, Long changingCafeId);
+	//
+	// StudyMemberListResponse findStudyMembersById(Long studyOnceId);
+	//
+	// StudyOnceResponse findStudyOnce(Long studyOnceId, LocalDateTime now);
+	//
+	// boolean doesOnlyStudyLeaderExist(Long studyOnceId);
+	//
+	// boolean isStudyOnceLeader(Long memberId, Long studyOnceId);
+	//
+	// void updateStudyOnce(long requestedMemberId, long studyOnceId, StudyOnceUpdateRequest request, LocalDateTime now);
+	//
+	// void updateStudyOncePartially(long requestedMemberId, long studyOnceId, StudyOnceUpdateRequest request);
+}
