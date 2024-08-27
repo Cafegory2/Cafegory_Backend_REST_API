c02c6dc3a6feed01a8da9c3afcdefc1cbe744c99
doha <dohaahi@naver.com>
2024-08-27 23:41:50 +0900

    test: validateStartDateTime 메서드 테스트 케이스 추가

--- src/test/java/com/example/demo/config/TestContainer.java
+++ src/test/java/com/example/demo/config/TestContainer.java
@@ -1,22 +1,22 @@
-//package com.example.demo.config;
-//
-//import org.springframework.test.context.DynamicPropertyRegistry;
-//import org.springframework.test.context.DynamicPropertySource;
-//import org.testcontainers.containers.MariaDBContainer;
-//
-//public abstract class TestContainer {
-//
-//	protected static MariaDBContainer mariadbContainer;
-//
-//	static {
-//		mariadbContainer = new MariaDBContainer("mariadb:10.11");
-//		mariadbContainer.start();
-//	}
-//
-//	@DynamicPropertySource
-//	protected static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-//		registry.add("spring.datasource.url", mariadbContainer::getJdbcUrl);
-//		registry.add("spring.datasource.username", mariadbContainer::getUsername);
-//		registry.add("spring.datasource.password", mariadbContainer::getPassword);
-//	}
-//}
+package com.example.demo.config;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MariaDBContainer;
+
+public abstract class TestContainer {
+
+	protected static MariaDBContainer mariadbContainer;
+
+	static {
+		mariadbContainer = new MariaDBContainer("mariadb:10.11");
+		mariadbContainer.start();
+	}
+
+	@DynamicPropertySource
+	protected static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mariadbContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", mariadbContainer::getUsername);
+		registry.add("spring.datasource.password", mariadbContainer::getPassword);
+	}
+}
