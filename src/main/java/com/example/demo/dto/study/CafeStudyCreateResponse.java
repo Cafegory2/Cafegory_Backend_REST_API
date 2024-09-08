03117c3f5bfe87e1767b088e2617d98ea809706e
doha <dohaahi@naver.com>
2024-09-08 13:25:36 +0900

    refactor: CafeStudyCreateResponse 생성자 레벨 Builer 사용

--- src/main/java/com/example/demo/dto/study/CafeStudyCreateResponse.java
+++ src/main/java/com/example/demo/dto/study/CafeStudyCreateResponse.java
@@ -5,11 +5,13 @@ import java.time.LocalDateTime;
 import com.example.demo.implement.study.MemberComms;
 import com.example.demo.implement.study.RecruitmentStatus;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CafeStudyCreateResponse {
 	private long cafeStudyId;
 	private String name;
@@ -23,4 +25,22 @@ public class CafeStudyCreateResponse {
 	private String introduction;
 	private int views;
 	private RecruitmentStatus recruitmentStatus;
+
+	@Builder
+	private CafeStudyCreateResponse(long cafeStudyId, String name, long cafeId, long coordinatorId,
+		LocalDateTime startDateTime, LocalDateTime endDateTime, MemberComms memberComms, int maxParticipants,
+		int nowParticipants, String introduction, int views, RecruitmentStatus recruitmentStatus) {
+		this.cafeStudyId = cafeStudyId;
+		this.name = name;
+		this.cafeId = cafeId;
+		this.coordinatorId = coordinatorId;
+		this.startDateTime = startDateTime;
+		this.endDateTime = endDateTime;
+		this.memberComms = memberComms;
+		this.maxParticipants = maxParticipants;
+		this.nowParticipants = nowParticipants;
+		this.introduction = introduction;
+		this.views = views;
+		this.recruitmentStatus = recruitmentStatus;
+	}
 }
