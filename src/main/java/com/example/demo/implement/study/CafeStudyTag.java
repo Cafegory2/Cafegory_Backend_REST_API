01bb3578ea1ba4c6b3de8a509cd5722927e77a28
doha <dohaahi@naver.com>
2024-09-08 13:51:47 +0900

    refactor: CafeStudyTag 코드 컨벤션 적용에 따른 변경

--- src/main/java/com/example/demo/implement/study/CafeStudyTag.java
+++ src/main/java/com/example/demo/implement/study/CafeStudyTag.java
@@ -15,14 +15,10 @@ import com.example.demo.implement.BaseEntity;
 import com.example.demo.implement.cafe.Tag;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "cafe_study_tag")
