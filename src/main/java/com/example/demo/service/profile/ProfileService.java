aaf0ebed40e84658f2afa2c2eb4e27ea6cea02d3
doha <dohaahi@naver.com>
2024-09-08 12:34:43 +0900

    chore: ProfileService interface 추석처리

--- src/main/java/com/example/demo/service/profile/ProfileService.java
+++ src/main/java/com/example/demo/service/profile/ProfileService.java
@@ -1,23 +1,13 @@
 package com.example.demo.service.profile;
 
-import static com.example.demo.util.TruncatedTimeUtil.*;
-
-import java.time.LocalDateTime;
-
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.demo.dto.profile.ProfileGetResponse;
-import com.example.demo.dto.profile.ProfileUpdateRequest;
-import com.example.demo.dto.profile.ProfileUpdateResponse;
-
 public interface ProfileService {
 
-	@Transactional
-	default ProfileGetResponse get(Long requestMemberId, Long targetMemberId) {
-		return get(requestMemberId, targetMemberId, LOCAL_DATE_TIME_NOW);
-	}
-
-	ProfileGetResponse get(Long requestMemberId, Long targetMemberId, LocalDateTime baseDateTime);
-
-	ProfileUpdateResponse update(Long requestMemberId, Long targetMemberId, ProfileUpdateRequest profileUpdateRequest);
+	// @Transactional
+	// default ProfileGetResponse get(Long requestMemberId, Long targetMemberId) {
+	// 	return get(requestMemberId, targetMemberId, LOCAL_DATE_TIME_NOW);
+	// }
+	//
+	// ProfileGetResponse get(Long requestMemberId, Long targetMemberId, LocalDateTime baseDateTime);
+	//
+	// ProfileUpdateResponse update(Long requestMemberId, Long targetMemberId, ProfileUpdateRequest profileUpdateRequest);
 }
