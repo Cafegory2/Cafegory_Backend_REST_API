cdb339bf6d6523733c5758f70266e157f2f9566f
kimdonghyun <ghffu405@gmail.com>
2024-09-08 23:19:03 +0900

    feat: login 패키지 생성 및 Login 클래스 생성

---
+++ src/main/java/com/example/demo/implement/login/Login.java
@@ -0,0 +1,13 @@
+package com.example.demo.implement.login;
+
+import com.example.demo.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Login {
+
+    private final MemberRepository memberRepository;
+
+
+
+}
