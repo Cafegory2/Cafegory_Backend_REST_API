e6c3d9192efb13e3032a9f1beaedd9496e0bb51f
kimdonghyun <ghffu405@gmail.com>
2024-09-19 19:39:34 +0900

    feat: 회원가입 환영 페이지 API 구현

--- src/main/java/com/example/demo/service/profile/ProfileService.java
+++ src/main/java/com/example/demo/service/profile/ProfileService.java
@@ -1,23 +1,123 @@
 package com.example.demo.service.profile;
 
-import static com.example.demo.util.TruncatedTimeUtil.*;
+import com.example.demo.dto.profile.WelcomeProfileResponse;
+import com.example.demo.implement.member.MemberReader;
+import com.example.demo.mapper.ProfileMapper;
+import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import com.example.demo.implement.member.Member;
 
-import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
 
-import com.example.demo.dto.profile.ProfileGetResponse;
-import com.example.demo.dto.profile.ProfileUpdateRequest;
-import com.example.demo.dto.profile.ProfileUpdateResponse;
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
 
-public interface ProfileService {
+    private final MemberReader memberReader;
+    private final ProfileMapper profileMapper;
 
-	@Transactional
-	default ProfileGetResponse get(Long requestMemberId, Long targetMemberId) {
-		return get(requestMemberId, targetMemberId, LOCAL_DATE_TIME_NOW);
-	}
+    public WelcomeProfileResponse getWelcomeProfile(Long memberId) {
+        Member member = memberReader.read(memberId);
+        return profileMapper.toWelcomeProfileResponse(member);
+    }
 
-	ProfileGetResponse get(Long requestMemberId, Long targetMemberId, LocalDateTime baseDateTime);
 
-	ProfileUpdateResponse update(Long requestMemberId, Long targetMemberId, ProfileUpdateRequest profileUpdateRequest);
+// 	@Override
+// 	public ProfileGetResponse get(Long requestMemberId, Long targetMemberId, LocalDateTime baseDateTime) {
+// 		if (isOwnerOfProfile(requestMemberId, targetMemberId)) {
+// 			return makeProfileGetResponse(targetMemberId);
+// 		}
+// 		if (isAllowedCauseStudyLeader(requestMemberId, targetMemberId)) {
+// 			return makeProfileGetResponse(targetMemberId);
+// 		}
+// 		if (isAllowedCauseSameStudyOnceMember(requestMemberId, targetMemberId, baseDateTime)) {
+// 			return makeProfileGetResponse(targetMemberId);
+// 		}
+// 		throw new CafegoryException(PROFILE_GET_PERMISSION_DENIED);
+// 	}
+//
+// 	@Override
+// 	public ProfileUpdateResponse update(Long requestMemberId, Long targetMemberId,
+// 		ProfileUpdateRequest profileUpdateRequest) {
+// 		validateProfileUpdatePermission(requestMemberId, targetMemberId);
+// 		Member targetMember = findTargetMember(targetMemberId);
+// 		String name = profileUpdateRequest.getName();
+// 		String introduction = profileUpdateRequest.getIntroduction();
+// 		targetMember.updateProfile(name, introduction);
+// 		return makeProfileUpdateResponse(targetMemberId);
+// 	}
+//
+// 	private void validateProfileUpdatePermission(Long requestMemberId, Long targetMemberId) {
+// 		if (!isOwnerOfProfile(requestMemberId, targetMemberId)) {
+// 			throw new CafegoryException(PROFILE_UPDATE_PERMISSION_DENIED);
+// 		}
+// 	}
+//
+// 	private Member findTargetMember(Long targetMemberId) {
+// 		Optional<Member> targetMember = memberRepository.findById(targetMemberId);
+// 		if (targetMember.isEmpty()) {
+// 			throw new CafegoryException(MEMBER_NOT_FOUND);
+// 		}
+// 		return targetMember.get();
+// 	}
+//
+// 	private ProfileGetResponse makeProfileGetResponse(Long targetMemberID) {
+// 		Member member = memberRepository.findById(targetMemberID).orElseThrow();
+// 		return new ProfileGetResponse(member.getName(), member.getThumbnailImage().getThumbnailImage(),
+// 			member.getIntroduction());
+// 	}
+//
+// 	private ProfileUpdateResponse makeProfileUpdateResponse(Long targetMemberID) {
+// 		Member member = memberRepository.findById(targetMemberID).orElseThrow();
+// 		return new ProfileUpdateResponse(member.getName(), member.getThumbnailImage().getThumbnailImage(),
+// 			member.getIntroduction());
+// 	}
+//
+// 	private boolean isOwnerOfProfile(Long requestMemberID, Long targetMemberID) {
+// 		if (requestMemberID == null || targetMemberID == null) {
+// 			return false;
+// 		}
+// 		return requestMemberID.equals(targetMemberID);
+// 	}
+//
+// 	private boolean isAllowedCauseStudyLeader(Long requestMemberID, Long targetMemberID) {
+// 		List<CafeStudy> cafeStudyByLeaderID = findStudyOnceByLeaderID(requestMemberID);
+// 		Set<Long> memberIdInStudyOnce = getMemberIdInStudyOnce(cafeStudyByLeaderID);
+// 		return memberIdInStudyOnce.contains(targetMemberID);
+// 	}
+//
+// 	private List<CafeStudy> findStudyOnceByLeaderID(Long requestMemberID) {
+// 		return studyOnceRepository.findByLeaderId(requestMemberID);
+// 	}
+//
+// 	private Set<Long> getMemberIdInStudyOnce(List<CafeStudy> byLeaderId) {
+// 		return mapToMemberId(byLeaderId.stream()
+// 			.flatMap(studyOnce -> studyOnce.getStudyMembers().stream())
+// 			.collect(Collectors.toList()));
+// 	}
+//
+// 	private static Set<Long> mapToMemberId(List<CafeStudyMember> cafeStudyMembers) {
+// 		return cafeStudyMembers.stream()
+// 			.map(CafeStudyMember::getMember)
+// 			.map(Member::getId)
+// 			.collect(Collectors.toSet());
+// 	}
+//
+// 	private boolean isAllowedCauseSameStudyOnceMember(Long requestMemberID, Long targetMemberID, LocalDateTime base) {
+// 		Member requestMember = memberRepository.findById(requestMemberID).orElseThrow();
+// 		List<CafeStudyMember> cafeStudyMembers = findAllSameStudyOnceStudyMembersWith(requestMember, base);
+// 		Set<Long> memberIdsThatJoinWithRequestMember = mapToMemberId(cafeStudyMembers);
+// 		return memberIdsThatJoinWithRequestMember.contains(targetMemberID);
+// 	}
+//
+// 	private List<CafeStudyMember> findAllSameStudyOnceStudyMembersWith(Member requestMember, LocalDateTime base) {
+// 		LocalDate baseDate = LocalDate.from(base);
+// 		return studyMemberRepository.findByMemberAndStudyDate(requestMember, baseDate)
+// 			.stream()
+// 			.map(CafeStudyMember::getStudy)
+// 			.filter(studyOnce -> !studyOnce.canJoin(base))
+// 			.filter(studyOnce -> !studyOnce.isEnd())
+// 			.flatMap(studyOnce -> studyOnce.getStudyMembers().stream())
+// 			.collect(Collectors.toList());
+// 	}
 }
---
+++ src/test/java/com/example/demo/mapper/ProfileMapperTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.profile.WelcomeProfileResponse;
+import com.example.demo.implement.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.example.demo.factory.TestMemberFactory.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileMapperTest {
+
+    private final ProfileMapper sut = new ProfileMapper();
+
+    @Test
+    @DisplayName("WelcomeProfileResponse 로 변환한다.")
+    void toWelcomeProfileResponse() {
+        //given
+        Member member = createMember("테스트닉네임", "testProfileUrl");
+        //when
+        WelcomeProfileResponse response = sut.toWelcomeProfileResponse(member);
+        //then
+        assertAll(
+            () -> assertThat(response.getNickname()).isEqualTo("테스트닉네임"),
+            () -> assertThat(response.getProfileUrl()).isEqualTo("testProfileUrl")
+        );
+    }
+
+}
