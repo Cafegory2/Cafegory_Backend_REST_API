b4381e9de159ba977bd63d9479ff53150f3f18f2
kimdonghyun <ghffu405@gmail.com>
2024-09-20 22:09:36 +0900

    test: RestAssured 로 Api 문서 작성

---
+++ src/test/java/com/example/demo/config/ApiDocsTest.java
@@ -0,0 +1,44 @@
+package com.example.demo.config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("local")
+@ExtendWith(RestDocumentationExtension.class)
+public class ApiDocsTest {
+
+    @LocalServerPort
+    private int port;
+
+    protected RequestSpecification spec;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+    @Autowired
+    protected MemberSignupAcceptanceTestHelper memberSignupHelper;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider provider) {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+
+        databaseCleanup.execute();
+
+        this.spec = new RequestSpecBuilder().addFilter(documentationConfiguration(provider))
+            .build();
+    }
+}
