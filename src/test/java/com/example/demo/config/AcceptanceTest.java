871c861c65cb1a4eda84e5d1b9d4430c716b248f
kimdonghyun <ghffu405@gmail.com>
2024-09-20 17:25:34 +0900

    test: 회원가입 환영 페이지 인수 테스트 작성

---
+++ src/test/java/com/example/demo/config/AcceptanceTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("local")
+public class AcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+    @Autowired
+    protected MemberSignupAcceptanceTestHelper memberSignupHelper;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+
+        databaseCleanup.execute();
+    }
+}
