926f59c5c80e57af63b0ee3404393da0673ca669
kimdonghyun <ghffu405@gmail.com>
2024-09-22 02:34:26 +0900

    refactor: api test 코드 수정

--- src/test/java/com/example/demo/apidocs/LoginControllerApiTest.java
+++ src/test/java/com/example/demo/apidocs/LoginControllerApiTest.java
@@ -1,73 +1,36 @@
 package com.example.demo.apidocs;
 
-import com.epages.restdocs.apispec.ResourceSnippetParameters;
-import com.example.demo.controller.LoginController;
-import com.example.demo.implement.token.JwtToken;
-import com.example.demo.service.login.LoginServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
+import com.example.demo.config.ApiDocsTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
-import static com.epages.restdocs.apispec.ResourceDocumentation.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
-@AutoConfigureMockMvc
-@ExtendWith(RestDocumentationExtension.class)
-@WebMvcTest(LoginController.class)
-public class LoginControllerApiTest {
-
-    @Autowired
-    private WebApplicationContext context;
-    @MockBean
-    private LoginServiceImpl loginServiceImpl;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-            .apply(MockMvcRestDocumentation
-                .documentationConfiguration(restDocumentation)
-                .operationPreprocessors()
-                .withResponseDefaults(Preprocessors.prettyPrint()))
-            .build();
-    }
+public class LoginControllerApiTest extends ApiDocsTest {
 
     @Test
-    void kakao() throws Exception {
-        when(loginServiceImpl.socialLogin(any())).thenReturn(new JwtToken("access-token-value", "refresh-token-value"));
-
-        this.mockMvc.perform(
-                get("/login/kakao?code={code}", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIU")
-                    .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-            .andDo(
-                document("카카오 로그인",
-                    resource(ResourceSnippetParameters.builder()
-                        .description("로그인이 성공하면 JWT 액세스 토큰과 리프레시 토큰을 발급 받는다. https://kauth.kakao.com/oauth/authorize?client_id=dd715e41cd41949dc316c0243b964c44&redirect_uri=http://{domain}/login/kakao&response_type=code")
-                        .tag("Login")
-                        .requestParameters(parameterWithName("code").description("카카오 인증 코드"))
-                        .responseFields(
-                            fieldWithPath("accessToken").description("JWT 액세스 토큰"),
-                            fieldWithPath("refreshToken").description("JWT 리프레시 토큰")
-                        )
-                        .build())));
+    void refresh() {
+        RestAssured.given(spec).log().all()
+            .filter(RestAssuredRestDocumentationWrapper.document(
+                    "카카오 로그인 API",
+                    requestParameters(
+                        parameterWithName("code").description("카카오 인증 code")
+                    ),
+                    responseFields(
+                        fieldWithPath("accessToken").description("JWT 액세스 토큰"),
+                        fieldWithPath("refreshToken").description("JWT 리프레시 토큰")
+                    )
+                )
+            )
+            .contentType(ContentType.JSON)
+            .param("code", "kakaoCode")
+            .when()
+            .get("/login/kakao")
+            .then().log().all()
+            .statusCode(200);
     }
 }
--- src/test/java/com/example/demo/apidocs/ProfileControllerApiTest.java
+++ src/test/java/com/example/demo/apidocs/ProfileControllerApiTest.java
@@ -1,80 +1,42 @@
 package com.example.demo.apidocs;
 
-import com.epages.restdocs.apispec.ResourceSnippetParameters;
-import com.example.demo.controller.ProfileController;
-import com.example.demo.dto.profile.WelcomeProfileResponse;
-import com.example.demo.service.profile.ProfileService;
-import org.junit.jupiter.api.BeforeEach;
+import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
+import com.example.demo.config.ApiDocsTest;
+import com.example.demo.implement.token.JwtToken;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
-import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
-@AutoConfigureMockMvc
-@ExtendWith(RestDocumentationExtension.class)
-@WebMvcTest(ProfileController.class)
-public class ProfileControllerApiTest {
-
-    @Autowired
-    private WebApplicationContext context;
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private ProfileService profileService;
-
-    @BeforeEach
-    void setUp(RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-            .apply(MockMvcRestDocumentation
-                .documentationConfiguration(restDocumentation)
-                .operationPreprocessors()
-                .withResponseDefaults(Preprocessors.prettyPrint()))
-            .build();
-    }
+public class ProfileControllerApiTest extends ApiDocsTest {
 
     @Test
-    @WithMockUser(username = "1")
-    void welcome() throws Exception {
-        when(profileService.getWelcomeProfile(1L))
-            .thenReturn(new WelcomeProfileResponse("테스트닉네임", "testProfileUrl"));
+    void welcome() {
+        JwtToken jwtToken = memberSignupHelper.로그인_되어_있음();
 
-        this.mockMvc.perform(
-                get("/profile/welcome")
-                    .header("Authorization", "Bearer existing-access-token")
-                    .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andDo(
-                document("회원가입 환영 페이지 API",
-                    resource(ResourceSnippetParameters.builder()
-                        .description("회원가입 환영 페이지에 알맞는 응답 데이터를 받는다.")
-                        .tag("Profile")
-                        .requestHeaders(
-                            headerWithName("Authorization").description("JWT 액세스 토큰")
-                        )
-                        .responseFields(
-                            fieldWithPath("nickname").description("회원 닉네임"),
-                            fieldWithPath("profileUrl").description("회원 프로필 이미지 url")
-                        )
-                        .build())));
+        RestAssured.given(spec).log().all()
+            .filter(RestAssuredRestDocumentationWrapper.document(
+                    "회원가입 환영 페이지 API",
+                    requestHeaders(
+                        headerWithName("Authorization").description("JWT 액세스 토큰"),
+                        headerWithName("Refresh-Token").description("JWT 리프레시 토큰, 리프레시 토큰 앞에는 Bearer을 붙이지 않는다.")
+                    ),
+                    responseFields(
+                        fieldWithPath("nickname").description("회원 닉네임"),
+                        fieldWithPath("profileUrl").description("회원 프로필 URL")
+                    )
+                )
+            )
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer " + jwtToken.getAccessToken())
+            .header("Refresh-Token", jwtToken.getRefreshToken())
+            .when()
+            .get("/profile/welcome")
+            .then().log().all()
+            .statusCode(200);
     }
 }
