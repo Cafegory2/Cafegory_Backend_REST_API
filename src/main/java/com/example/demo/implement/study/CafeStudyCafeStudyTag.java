17a4ab4e3d2eb936f0fba3e3cc664a7ec9adf5b6
kimdonghyun <ghffu405@gmail.com>
2024-09-27 20:51:16 +0900

    refactor: CafeStudyCafeStudyTag 클래스 복구

---
+++ src/main/java/com/example/demo/implement/study/CafeStudyCafeStudyTag.java
@@ -0,0 +1,28 @@
+package com.example.demo.implement.study;
+
+import com.example.demo.implement.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "cafe_study_cafe_study_tag")
+public class CafeStudyCafeStudyTag extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cafe_study_cafe_study_tag_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_study_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private CafeStudy cafeStudy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_study_tag_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private CafeStudyTag cafeStudyTag;
+}
