d39b7372ba82e45bf60360587a6909a4268c7680
kimdonghyun <ghffu405@gmail.com>
2024-10-11 18:10:52 +0900

    feat: 카공 상세 정보 조회 기능 구현

---
+++ src/main/java/com/example/demo/implement/study/CafeStudyCommentReader.java
@@ -0,0 +1,18 @@
+package com.example.demo.implement.study;
+
+import com.example.demo.repository.study.CafeStudyCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CafeStudyCommentReader {
+
+    private final CafeStudyCommentRepository cafeStudyCommentRepository;
+
+    public List<CafeStudyComment> readAllBy(Long cafeStudyId) {
+        return cafeStudyCommentRepository.findAllBy(cafeStudyId);
+    }
+}
