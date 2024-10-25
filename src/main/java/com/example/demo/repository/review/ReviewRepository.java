7e9f3a898a0b71c149a86b2f1fe5df2238f6177c
kimdonghyun <ghffu405@gmail.com>
2024-10-25 20:09:55 +0900

    donghyun pair4

--- src/main/java/com/example/demo/repository/review/ReviewRepository.java
+++ src/main/java/com/example/demo/repository/review/ReviewRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.implement.review.ReviewEntity;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
 
+	@Query("select distinct r from ReviewEntity r" +
+		" inner join fetch r.cafe" +
+		" left join fetch r.reviewCafeTag")
 	List<ReviewEntity> findAllByMemberId(Long memberId);
-	
 }
