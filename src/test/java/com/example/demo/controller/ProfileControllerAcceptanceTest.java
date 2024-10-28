078a30bc2c5454057afa0b90888ac25fc48bb39e
kimdonghyun <ghffu405@gmail.com>
2024-10-28 17:51:00 +0900

    test: 카공 Qna 댓글 저장 테스크 케이스 추가

--- src/test/java/com/example/demo/controller/ProfileControllerAcceptanceTest.java
+++ src/test/java/com/example/demo/controller/ProfileControllerAcceptanceTest.java
@@ -9,7 +9,6 @@ import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.*;
---
+++ src/test/java/com/example/demo/qna/implement/CommentAppenderTest.java
@@ -0,0 +1,89 @@
+package com.example.demo.qna.implement;
+
+import com.example.demo.config.ServiceTest;
+import com.example.demo.helper.CafeSaveHelper;
+import com.example.demo.helper.CafeStudyCommentSaveHelper;
+import com.example.demo.helper.CafeStudySaveHelper;
+import com.example.demo.helper.MemberSaveHelper;
+import com.example.demo.implement.cafe.CafeEntity;
+import com.example.demo.implement.member.MemberEntity;
+import com.example.demo.implement.study.CafeStudyEntity;
+import com.example.demo.implement.study.StudyRole;
+import com.example.demo.member.domain.MemberIdentity;
+import com.example.demo.qna.domain.Comment;
+import com.example.demo.qna.repository.CafeStudyCommentEntity;
+import com.example.demo.util.TimeUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CommentAppenderTest extends ServiceTest {
+
+    @Autowired
+    private CommentAppender sut;
+    @Autowired
+    private CafeStudySaveHelper cafeStudySaveHelper;
+    @Autowired
+    private MemberSaveHelper memberSaveHelper;
+    @Autowired
+    private CafeSaveHelper cafeSaveHelper;
+    @Autowired
+    private CafeStudyCommentSaveHelper cafeStudyCommentSaveHelper;
+    @Autowired
+    private TimeUtil timeUtil;
+
+    @Test
+    @DisplayName("댓글을 저장한다.")
+    void save_question() {
+        //given
+        CafeEntity cafe = cafeSaveHelper.saveCafe();
+
+        MemberEntity member = memberSaveHelper.saveMember();
+
+        LocalDateTime dateTime = timeUtil.localDateTime(2000, 1, 1, 12, 0, 0);
+        CafeStudyEntity cafeStudy = cafeStudySaveHelper.saveCafeStudy(cafe, member, dateTime, dateTime.plusHours(2));
+
+        Comment comment = createComment(cafeStudy, member, null);
+        //when
+        Long savedMemberId = sut.append(comment, member.getId());
+        //then
+        assertThat(savedMemberId).isNotNull();
+    }
+
+    @Test
+    @DisplayName("대댓글을 저장한다.")
+    void save_reply() {
+        //given
+        CafeEntity cafe = cafeSaveHelper.saveCafe();
+
+        MemberEntity coordinator = memberSaveHelper.saveMember("coordinator@gmail.com");
+        MemberEntity member = memberSaveHelper.saveMember("member@gmail.com");
+
+        LocalDateTime dateTime = timeUtil.localDateTime(2000, 1, 1, 12, 0, 0);
+        CafeStudyEntity cafeStudy = cafeStudySaveHelper.saveCafeStudy(cafe, member, dateTime, dateTime.plusHours(2));
+        CafeStudyCommentEntity rootComment = cafeStudyCommentSaveHelper.saveRootComment(member, StudyRole.MEMBER, cafeStudy);
+
+        Comment comment = createComment(cafeStudy, coordinator, rootComment);
+        //when
+        Long savedMemberId = sut.append(comment, member.getId());
+        //then
+        assertThat(savedMemberId).isNotNull();
+    }
+
+    private Comment createComment(
+        CafeStudyEntity cafeStudy, MemberEntity member, CafeStudyCommentEntity parentCommentEntity) {
+        return Comment.builder()
+            .cafeStudyId(cafeStudy.getId())
+            .member(
+                MemberIdentity.builder()
+                    .id(member.getId())
+                    .nickname(member.getNickname())
+                    .build()
+            )
+            .build();
+    }
+}
