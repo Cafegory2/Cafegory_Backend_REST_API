d2fc01151c80d1fdceecd0ac1b103077305a362c
kimdonghyun <ghffu405@gmail.com>
2024-12-18 21:55:02 +0900

    refactor: Qna 테스트 코드 리팩터링

--- src/test/java/com/example/demo/qna/implement/CommentEditorTest.java
+++ src/test/java/com/example/demo/qna/implement/CommentEditorTest.java
@@ -12,31 +12,25 @@ import com.example.demo.qna.domain.Comment;
 import com.example.demo.qna.domain.CommentContent;
 import com.example.demo.qna.infrastructure.CafeStudyCommentEntity;
 import com.example.demo.qna.infrastructure.CafeStudyCommentRepository;
-import com.example.demo.study.domain.StudyRole;
 import com.example.demo.study.infrastructure.CafeStudyEntity;
 import com.example.demo.util.TimeUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.example.demo.study.domain.StudyRole.*;
+import static com.example.demo.testbuilder.CafeBuilder.*;
+import static com.example.demo.testbuilder.CommentBuilder.*;
+import static com.example.demo.testbuilder.MemberBuilder.*;
+import static com.example.demo.testbuilder.StudyBuilder.*;
 import static org.assertj.core.api.Assertions.*;
 
 class CommentEditorTest extends ServiceTest {
 
     @Autowired
     private CommentEditor sut;
-
-    @Autowired
-    private CafeStudySaveHelper cafeStudySaveHelper;
-    @Autowired
-    private MemberSaveHelper memberSaveHelper;
-    @Autowired
-    private CafeSaveHelper cafeSaveHelper;
-    @Autowired
-    private CafeStudyCommentSaveHelper cafeStudyCommentSaveHelper;
 
     @Autowired
     private CafeStudyCommentRepository cafeStudyCommentRepository;
@@ -48,16 +42,13 @@ class CommentEditorTest extends ServiceTest {
     @DisplayName("댓글을 저장한다.")
     void save_question() {
         //given
-        CafeEntity cafe = cafeSaveHelper.saveCafe();
+        CafeEntity cafe = aCafe().save();
 
-        MemberEntity member = memberSaveHelper.saveMember();
-
-        LocalDateTime dateTime = timeUtil.localDateTime(2000, 1, 1, 12, 0, 0);
-        CafeStudyEntity cafeStudy = cafeStudySaveHelper.saveCafeStudy(cafe, member, dateTime, dateTime.plusHours(2));
-
-        Comment comment = createComment("댓글 내용", cafeStudy.getId(), member, null);
+        MemberEntity coordinator = aMember().asCoordinator().save();
+        CafeStudyEntity study = aStudy().with(cafe).with(coordinator).save();
+        Comment comment = createComment("댓글 내용", study.getId(), coordinator, null);
         //when
-        Long savedCommentId = sut.save(comment, member.getId());
+        Long savedCommentId = sut.save(comment, coordinator.getId());
         //then
         assertThat(savedCommentId).isNotNull();
     }
@@ -66,16 +57,14 @@ class CommentEditorTest extends ServiceTest {
     @DisplayName("대댓글을 저장한다.")
     void save_reply() {
         //given
-        CafeEntity cafe = cafeSaveHelper.saveCafe();
+        CafeEntity cafe = aCafe().save();
 
-        MemberEntity coordinator = memberSaveHelper.saveMember("coordinator@gmail.com");
-        MemberEntity member = memberSaveHelper.saveMember("member@gmail.com");
+        MemberEntity coordinator = aMember().asCoordinator().save();
+        MemberEntity member = aMember().asParticipant().save();
 
-        LocalDateTime dateTime = timeUtil.localDateTime(2000, 1, 1, 12, 0, 0);
-        CafeStudyEntity cafeStudy = cafeStudySaveHelper.saveCafeStudy(cafe, member, dateTime, dateTime.plusHours(2));
-        CafeStudyCommentEntity rootComment = cafeStudyCommentSaveHelper.saveRootComment(member, StudyRole.MEMBER, cafeStudy);
-
-        Comment comment = createComment("대댓글 내용", cafeStudy.getId(), coordinator, rootComment.getId());
+        CafeStudyEntity study = aStudy().with(cafe).with(coordinator).save();
+        CafeStudyCommentEntity rootComment = aComment().with(study).with(member).withStudyRole(MEMBER).save();
+        Comment comment = createComment("대댓글 내용", study.getId(), coordinator, rootComment.getId());
         //when
         Long savedCommentId = sut.save(comment, member.getId());
         //then
@@ -105,17 +94,13 @@ class CommentEditorTest extends ServiceTest {
     @DisplayName("댓글을 수정한다.")
     void edit_comment() {
         //given
-        CafeEntity cafe = cafeSaveHelper.saveCafeWith7daysFrom9To21();
+        CafeEntity cafe = aCafe().saveWith7daysFrom9To21();
 
-        MemberEntity coordinator = memberSaveHelper.saveMember("coordinator@gmail.com");
-        MemberEntity member = memberSaveHelper.saveMember("member@gmail.com");
+        MemberEntity coordinator = aMember().asCoordinator().save();
+        MemberEntity member = aMember().asParticipant().save();
 
-        LocalDateTime startDateTime =
-            timeUtil.localDateTime(2000, 1, 1, 10, 0, 0);
-        CafeStudyEntity cafeStudy =
-            cafeStudySaveHelper.saveCafeStudy(cafe, coordinator, startDateTime, startDateTime.plusHours(2));
-        CafeStudyCommentEntity commentEntity =
-            cafeStudyCommentSaveHelper.saveRootComment(member, StudyRole.MEMBER, cafeStudy);
+        CafeStudyEntity study = aStudy().with(cafe).with(coordinator).save();
+        CafeStudyCommentEntity commentEntity = aComment().with(study).with(member).withStudyRole(MEMBER).save();
         Comment comment = createComment("변경된 댓글 내용", commentEntity.getId());
         //when
         sut.edit(comment);
@@ -139,17 +124,13 @@ class CommentEditorTest extends ServiceTest {
     @DisplayName("댓글을 삭제한다.")
     void remove_comment() {
         //given
-        CafeEntity cafe = cafeSaveHelper.saveCafeWith7daysFrom9To21();
+        CafeEntity cafe = aCafe().saveWith7daysFrom9To21();
 
-        MemberEntity coordinator = memberSaveHelper.saveMember("coordinator@gmail.com");
-        MemberEntity member = memberSaveHelper.saveMember("member@gmail.com");
+        MemberEntity coordinator = aMember().asCoordinator().save();
+        MemberEntity member = aMember().asParticipant().save();
 
-        LocalDateTime startDateTime =
-            timeUtil.localDateTime(2000, 1, 1, 10, 0, 0);
-        CafeStudyEntity cafeStudy =
-            cafeStudySaveHelper.saveCafeStudy(cafe, coordinator, startDateTime, startDateTime.plusHours(2));
-        CafeStudyCommentEntity commentEntity =
-            cafeStudyCommentSaveHelper.saveRootComment(member, StudyRole.MEMBER, cafeStudy);
+        CafeStudyEntity study = aStudy().with(cafe).with(coordinator).save();
+        CafeStudyCommentEntity commentEntity = aComment().with(study).with(member).withStudyRole(MEMBER).save();
         //when
         sut.remove(commentEntity.getId(), timeUtil.now());
         //then
